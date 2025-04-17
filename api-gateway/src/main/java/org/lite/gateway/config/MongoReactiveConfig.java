6da44ed29f554e64eb89f00bea3f1951e8480e3f
mehmetsen <mehmetsen80@gmail.com>
2025-04-16 23:45:23 -0500

    Dockerized configuration, fixed bugs

--- api-gateway/src/main/java/org/lite/gateway/config/MongoReactiveConfig.java
+++ api-gateway/src/main/java/org/lite/gateway/config/MongoReactiveConfig.java
@@ -2,6 +2,9 @@ package org.lite.gateway.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +26,7 @@ import org.springframework.transaction.reactive.TransactionalOperator;
 @Configuration
 @EnableReactiveMongoRepositories(basePackages = "org.lite.gateway.repository")
 @EnableReactiveMongoAuditing
+@Slf4j
 public class MongoReactiveConfig extends AbstractReactiveMongoConfiguration {
 
     @Value("${spring.data.mongodb.uri}")

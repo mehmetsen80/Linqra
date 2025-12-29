63063dd4c1eb736a626e57661f78fbe9e7dd007d
mehmetsen <mehmetsen80@gmail.com>
2025-12-29 12:44:34 -0600

    Removed the Redis container from ec2 setup

--- api-gateway/src/main/java/org/lite/gateway/config/RedisConfig.java
+++ api-gateway/src/main/java/org/lite/gateway/config/RedisConfig.java
@@ -2,36 +2,18 @@ package org.lite.gateway.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.lite.gateway.listener.CustomMessageListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Slf4j
 public class RedisConfig {
-
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @Bean
-    public ReactiveStringRedisTemplate reactiveStringRedisTemplate(
-            @Qualifier("redisConnectionFactory") RedisConnectionFactory connectionFactory) {
-        return new ReactiveStringRedisTemplate((ReactiveRedisConnectionFactory) connectionFactory);
-    }
 
     // Define the Redis Pub/Sub topic for route updates
     @Bean
@@ -57,16 +39,6 @@ public class RedisConfig {
         MessageListenerAdapter adapter = new MessageListenerAdapter(customMessageListener, "handleMessage");
         adapter.setSerializer(new StringRedisSerializer());
         return adapter;
-    }
-
-    // Configure Redis connection factory using injected host and port
-    @Bean
-    @Primary
-    public RedisConnectionFactory redisConnectionFactory() {
-        log.info("Connecting to Redis at {}:{}", redisHost, redisPort);
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
-        factory.afterPropertiesSet();
-        return factory;
     }
 
     // Configure RedisTemplate with String serializer
